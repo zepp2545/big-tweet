0afa3970b0e03a847ea5905a5b9a37985f59a9dc
Konan Maruyama <zepp25458565@gmail.com>
2020-05-30 17:53:50 +0700

    added show method

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/confirm/:id',to: 'posts#confirm',as: :confirm
-  resources :posts,only: %i(create edit update)
+  resources :posts,only: %i(create edit update show)
   root to: "posts#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
